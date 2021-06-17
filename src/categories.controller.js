6e01e4e55c98bda624a2bf8b45a0955e1a8530bd
Oleg Solomin <solomin.o.n@gmail.com>
2021-06-17 18:20:18 +0300

    some fixes but not working

--- src/categories.controller.js
+++ src/categories.controller.js
@@ -5,20 +5,20 @@ angular.module('MenuApp')
 .controller('CategoriesController', CategoriesController);
 
 // simple way - without resolve and ctrl in state
-CategoriesController.$inject = ['MenuDataService'];
-function CategoriesController(MenuDataService) {
-var ctrl = this;
-var promise = MenuDataService.getAllCategories();
-promise.then(function (response) {
-    ctrl.categories_list = response.data;
-})
-}
+// CategoriesController.$inject = ['MenuDataService'];
+// function CategoriesController(MenuDataService) {
+// var ctrl = this;
+// var promise = MenuDataService.getAllCategories();
+// promise.then(function (response) {
+//     ctrl.categories_list = response.data;
+// })
+// }
 
 //proper way
-CategoriesController.$inject = ['items'];
-function CategoriesController(items) {
+CategoriesController.$inject = ['MenuDataService','items'];
+function CategoriesController(MenuDataService,items) {
 var ctrl = this;
-ctrl.items = categories_list;
+ctrl.categories_list = items.data;
 }
 
 })();
